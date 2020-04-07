ea7a7f251fffa133d7b9a585e5591bf4b2d6a1f1
Camarillo <cjc282@miami.edu>
2020-04-07 01:52:16 -0400

    transparency

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,5 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCWVNbn7JtGV7PFmqExHY4aOsG-KJMsJJQ",
-    authDomain: "projectu-51ef9.firebaseapp.com",
-    databaseURL: "https://projectu-51ef9.firebaseio.com",
-    projectId: "projectu-51ef9",
-    storageBucket: "projectu-51ef9.appspot.com",
-    messagingSenderId: "225676322565",
-    appId: "1:225676322565:web:b72205ae8654fbe447796c",
-    measurementId: "G-LCFS9K4SDQ"
+    // go to console.firebase.google.com -> projectU -> settings icon -> 
+    // general -> scroll down to your apps -> Firebase SDK snippet -> Config
+    // copy the inside of firebaseConfig here and yarn start
 };

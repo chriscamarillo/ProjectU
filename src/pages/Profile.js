9890798dca9e60a05fdd1b9b357a33655fcf17de
Camarillo <cjc282@miami.edu>
2020-04-08 16:35:18 -0400

    final

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { UserContext } from '../components/UserProvider'
+import { db } from '../services/firebase'
 
 const Profile = () => {
-
+    
     return(
         <h1>under construction</h1>
     )

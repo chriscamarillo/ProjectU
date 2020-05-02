084d0ee2b6c9b573af2dec6ba1d41f0b3e862fb5
Melanie Xia <mxx70@miami.edu>
2020-05-02 18:13:56 -0400

    Editable Project and Project.css

--- src/components/frontend/forms/ProjectForm.js
+++ src/components/frontend/forms/ProjectForm.js
@@ -10,6 +10,25 @@ const ProjectForm = (props) =>{
             <form onSubmit={props.handleSubmit}>
                 <div class="form-group">
                     <label for="projectTitle">Project Title</label>
+                    <textarea type="text" id="titleInput" name="title" contentEditable="true" ref={props.register}>{project.title}</textarea>
+                </div>   
+                    <p>{props.errors.title && '*Project title is required'}</p>
+                <div class="form-group">
+                    <label for="projectDescription">Project Description</label>
+                    <textarea type="text" id="descriptionInput" name="description" contentEditable="true" ref={props.register}>{project.description}</textarea>
+                </div>
+                <p>{props.errors.description && '*Project description is required'}</p>
+                <button type ="submit" class="btn btn-primary">Submit</button>
+            </form> 
+        </div> 
+    )
+}
+/*
+  <div className="ProjectForm">
+        <h1>Edit Project</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div class="form-group">
+                    <label for="projectTitle">Project Title</label>
                     <textarea type="text" id="titleInput" name="title" placeholder={project.title} ref={props.register}></textarea>
                 </div>   
                     <p>{props.errors.title && '*Project title is required'}</p>
@@ -20,21 +39,6 @@ const ProjectForm = (props) =>{
                 <p>{props.errors.description && '*Project description is required'}</p>
                 <button type ="submit" class="btn btn-primary">Submit</button>
             </form> 
-        </div> 
-    )
-}
-/*
- <div>
-            <h3>Edit Project</h3>
-            <form onSubmit={props.handleSubmit}>
-                Title
-                <input type="text" name="title" placeholder={project.title} ref={props.register} />
-                <p>{props.errors.title && 'Project title is required'}</p>
-                Description
-                <input type="text" name="description" placeholder={project.description} ref={props.register} />
-                <p>{props.errors.description && 'Project description is required'}</p>
-                <input type ="submit" />
-            </form>
         </div>
 */
 export default ProjectForm

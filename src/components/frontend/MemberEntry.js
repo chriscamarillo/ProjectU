3506faa1c6806322fb69a03945711296d336071a
Galan <aig71@miami.edu>
2020-05-03 03:19:16 -0400

    adjusted dates

--- src/components/frontend/MemberEntry.js
+++ src/components/frontend/MemberEntry.js
@@ -3,13 +3,23 @@ import { db } from '../../services/firebase'
 
 const MemberEntry = (props) => {
     const member = props.member;
+    
+    const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sepr", "Oct", "Nov", "Dec"]
+    
+    let dateObj = member.date_added.toDate()
+    let date = (member.date_added) ? 
+                days[dateObj.getDay()] + ' ' +
+                months[dateObj.getMonth()] + ' ' +
+                dateObj.getDate() + ', ' + 
+                dateObj.getFullYear()
+                : '';
 
-    console.log('here are some members: ', member)
     return (
         (member) ?
         <div className="MemberEntry">
             <img src={member.photoURL}  alt="profile" width="100" height="100"></img>
-            <h2>{`${member.displayName} joined ${member.date_added}`}</h2>
+            <h2>{`${member.displayName} joined ${date}`}</h2>
         </div>:
         <h1>Loading...</h1> 
     )
